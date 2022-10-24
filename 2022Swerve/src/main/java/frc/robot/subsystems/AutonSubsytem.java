e102a86d65128178b92931d151e2e6ba9bf90c08
zaneb <zanebrotherton@gmail.com>
2022-10-24 09:50:07 -0500

    Add shuffleboard compatability

--- 2022Swerve/src/main/java/frc/robot/subsystems/AutonSubsytem.java
+++ 2022Swerve/src/main/java/frc/robot/subsystems/AutonSubsytem.java
@@ -7,8 +7,12 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -26,6 +30,10 @@ public class AutonSubsytem extends SubsystemBase{
         FORWARD180; // Go forward 2 meters and rotate 180 degrees
     }
 
+    private ShuffleboardTab configTab = Shuffleboard.getTab("Config");
+    private NetworkTableEntry delayEntry = configTab.add("Auton Delay", 0.0).getEntry();
+    private SendableChooser<AutonModes> modeChooser = new SendableChooser<>();
+
     private SwerveSubsystem swerveSubsystem;
 
     private PIDController xController;
@@ -36,6 +44,12 @@ public class AutonSubsytem extends SubsystemBase{
 
     public AutonSubsytem(SwerveSubsystem swerveSubsystem){
         this.swerveSubsystem = swerveSubsystem;
+
+        //Add auton modes to chooser
+        for(AutonModes mode : AutonModes.values()){
+            modeChooser.addOption(mode.toString(), mode);
+        }
+        configTab.add("Auton mode", modeChooser);
 
         //Define PID controllers for tracking trajectory
         xController = new PIDController(AutonConstants.kXTranslationP, 0, 0);
@@ -110,6 +124,6 @@ public class AutonSubsytem extends SubsystemBase{
     }
 
     public Command getAutonCommand(){
-        return getAutonSequence().andThen(getAutonEnd());
+        return getAutonSequence().beforeStarting(new WaitCommand(delayEntry.getDouble(0.0))).andThen(getAutonEnd());
     }
 }
