fc6bfc149d1d883bfcf1a2ecd61c8696e60838ad
zaneb <zanebrotherton@gmail.com>
2022-11-01 16:35:10 -0500

    Use PathPlannerLib for auton

--- 2022Swerve/src/main/java/frc/robot/subsystems/AutonSubsytem.java
+++ 2022Swerve/src/main/java/frc/robot/subsystems/AutonSubsytem.java
@@ -1,25 +1,24 @@
 package frc.robot.subsystems;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.Objects;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.util.datalog.StringLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -27,7 +26,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.AutonConstants;
 import frc.robot.Constants.DriveConstants;
@@ -53,9 +51,9 @@ public class AutonSubsytem extends SubsystemBase{
 
     private PIDController xController;
     private PIDController yController;
-    private ProfiledPIDController rotationController;
+    private PIDController rotationController;
 
-    private TrajectoryConfig trajectoryConfig;
+    private PathConstraints pathConstraints;
 
     private AutonModes autonMode;
 
@@ -72,49 +70,35 @@ public class AutonSubsytem extends SubsystemBase{
         //Define PID controllers for tracking trajectory
         xController = new PIDController(AutonConstants.kXTranslationP, 0, 0);
         yController = new PIDController(AutonConstants.kYTranslationP, 0, 0);
-        rotationController = new ProfiledPIDController(
-            AutonConstants.kRotationP, 0, 0, AutonConstants.kRotationalConstraints);
+        rotationController = new PIDController(AutonConstants.kRotationP, 0, 0);
         rotationController.enableContinuousInput(-Math.PI, Math.PI);
 
-        //Define config for generated trajectories
-        trajectoryConfig = new TrajectoryConfig(
-            AutonConstants.kMaxSpeed, AutonConstants.kMaxAcceleration).
-            setKinematics(DriveConstants.kDriveKinematics);
+        pathConstraints = new PathConstraints(AutonConstants.kMaxSpeed, AutonConstants.kMaxAcceleration);
     }
 
-    private Trajectory getTrajectory(String name) throws IOException{
-        Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("pathplanner/generatedJSON/" + name + ".wplib.json");
-        return TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    private PathPlannerTrajectory getTrajectory(String name) throws NullPointerException{
+        return PathPlanner.loadPath(name, pathConstraints);
     }
 
-    private Trajectory generateTrajectory(Pose2d initialPose, List<Translation2d> waypoints, Pose2d finalPose){
-        return TrajectoryGenerator.generateTrajectory(
-            initialPose,
-            waypoints,
-            finalPose,
-            trajectoryConfig
+    private PathPoint constructPoint(double x, double y, double rotation, double heading){
+        return new PathPoint(new Translation2d(x, y), Rotation2d.fromDegrees(heading), Rotation2d.fromDegrees(rotation));
+    }
+
+    private PathPlannerTrajectory generateTrajectory(PathPoint firstPoint, PathPoint secondPoint, PathPoint... points){
+        return PathPlanner.generatePath(
+            pathConstraints,
+            firstPoint,
+            secondPoint,
+            points
         );
     }
 
-    private SwerveControllerCommand followTrajectory(String name) throws IOException{
+    private PPSwerveControllerCommand followTrajectory(String name) throws NullPointerException{
         //For use with trajectories generated from pathplanner
         trajectoryLog.append("Following trajectory " + name);
-        return new SwerveControllerCommand(
-            getTrajectory(name),
-            swerveSubsystem::getPose, 
-            DriveConstants.kDriveKinematics, 
-            xController, 
-            yController, 
-            rotationController, 
-            swerveSubsystem::setModuleStates, 
-            swerveSubsystem
-        );
-    }
-
-    private SwerveControllerCommand followTrajectory(Trajectory trajectory){
-        //For use with trajectories generated from a list of poses
-        trajectoryLog.append("Following generated trajectory");
-        return new SwerveControllerCommand(
+        PathPlannerTrajectory trajectory = getTrajectory(name);
+        if(Objects.isNull(trajectory)){throw new NullPointerException();}
+        return new PPSwerveControllerCommand(
             trajectory,
             swerveSubsystem::getPose, 
             DriveConstants.kDriveKinematics, 
@@ -126,9 +110,24 @@ public class AutonSubsytem extends SubsystemBase{
         );
     }
 
-    private Command resetOdometry(String initialTrajectory) throws IOException{
+    private PPSwerveControllerCommand followTrajectory(PathPlannerTrajectory trajectory){
+        //For use with trajectories generated from a list of poses
+        trajectoryLog.append("Following generated trajectory");
+        return new PPSwerveControllerCommand(
+            trajectory,
+            swerveSubsystem::getPose, 
+            DriveConstants.kDriveKinematics, 
+            xController, 
+            yController, 
+            rotationController, 
+            swerveSubsystem::setModuleStates, 
+            swerveSubsystem
+        );
+    }
+
+    private Command resetOdometry(String initialTrajectory) throws NullPointerException{
         //Resets odometry to the initial position of the given trajectory
-        Trajectory trajectory = getTrajectory(initialTrajectory);
+        PathPlannerTrajectory trajectory = getTrajectory(initialTrajectory);
         Pose2d initialPose = Objects.isNull(trajectory) ? new Pose2d(0, 0, new Rotation2d()) : trajectory.getInitialPose();
         return new InstantCommand(() -> swerveSubsystem.resetOdometry(initialPose));
     }
@@ -145,8 +144,8 @@ public class AutonSubsytem extends SubsystemBase{
             case FORWARD:
                return 
                     new SequentialCommandGroup(
-                        resetOdometry("Forward"),
-                        followTrajectory("Forward")
+                        resetOdometry("foo"),
+                        followTrajectory("bar")
                     );
             case BACKWARD:
                 return
@@ -164,7 +163,7 @@ public class AutonSubsytem extends SubsystemBase{
             default:
                 return null;
         }
-        }catch(IOException e){
+        }catch(NullPointerException e){
             DriverStation.reportError("Was unable to access a trajectory", e.getStackTrace());
             return getBackupSequence();
         }
@@ -175,12 +174,8 @@ public class AutonSubsytem extends SubsystemBase{
         return new SequentialCommandGroup(
             followTrajectory(
                 generateTrajectory(
-                    new Pose2d(0, 0, new Rotation2d(0)),
-                    List.of(
-                        new Translation2d(1,0),
-                        new Translation2d(1,-1)
-                    ),
-                    new Pose2d(0, 0, Rotation2d.fromDegrees(180))
+                    constructPoint(0, 0, 0, 90),
+                    constructPoint(0, 1, 0, 0)
                 )
             )
         );
